51e3845297eb0d88020adafa61cb30f576f4fc0f
Pavel Ivanov <p.r.ivanov94@gmail.com>
2021-12-05 19:28:40 +0200

    Almost finished without 3rd parties

---
+++ managers/auth.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta
+from flask_httpauth import HTTPTokenAuth
+
+import jwt
+from decouple import config
+from werkzeug.exceptions import BadRequest
+
+from models.user import ComplainerModel, AdminModel, ApproverModel
+
+
+class AuthManager:
+    @staticmethod
+    def encode_token(user):
+        payload = {
+            "sub": user.id,
+            "exp": datetime.utcnow() + timedelta(days=100),
+            "role": user.__class__.__name__,
+        }
+        return jwt.encode(payload, key=config("JWT_KEY"), algorithm="HS256")
+
+    @staticmethod
+    def decode_token(token):
+        try:
+            data = jwt.decode(token, key=config("JWT_KEY"), algorithms=["HS256"])
+            return data["sub"], data["role"]
+        except jwt.ExpiredSignatureError:
+            raise BadRequest("Token expired")
+        except jwt.InvalidTokenError:
+            raise BadRequest("Invalid token")
+
+
+auth = HTTPTokenAuth(scheme="Bearer")
+
+
+@auth.verify_token
+def verify_token(token):
+    user_id, role = AuthManager.decode_token(token)
+    user = eval(f"{role}.query.filter_by(id={user_id}).first()")
+    return user
